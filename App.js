b688ea03e514e1f4cdc2ea73e378cf3d69b6d35c
sakshi1195 <sakshipanchal1195@gmail.com>
2025-04-08 16:23:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import WeatherA from './WeatherA';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Weather Application</h1>
+
+   <WeatherA/>
+    </div>
+  );
+}
+
+export default App;
